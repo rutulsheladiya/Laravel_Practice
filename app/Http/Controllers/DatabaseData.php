9fefa41377c5ac585b089f972c1c3d4f1f12d4dd
rutul sheladiya <rutul@simformsolutions.com>
2023-06-23 17:18:32 +0530

    queryBuilder Completed

--- app/Http/Controllers/DatabaseData.php
+++ app/Http/Controllers/DatabaseData.php
@@ -95,7 +95,7 @@ class DatabaseData extends Controller
         //dd($data);
 
         // distinct()
-        // $data = DB::table('employee')->distinct('Department')->get();
+        // $data = DB::table('employee')->select('Department')->distinct('Department')->get();
         // dd($data);
 
         // -------------------------------------------------------------------------
@@ -124,7 +124,7 @@ class DatabaseData extends Controller
 
 
         // orwhere => a orwhere clause or jevu j work karse
-        // $data = DB::table('employee')->where('EmployeeId', '=', '1001')->orWhere('Age', '=', 27)->get();
+        // $data = DB::table('employee')->select()->where('EmployeeId', '=', 1001)->orWhere('Age', '=', 27)->get();
         // dd($data);
 
         // wherebetween(colname,[range]) between operator jevu kam karse. -jeni age 225 thi 30 ni vacche che ae badha return karse.
@@ -173,9 +173,152 @@ class DatabaseData extends Controller
         // dd($data);
 
         //whereDay
-        // $data = DB::table('employee')->select()->whereDay('BirthDate', '25')->get();
+        // $data = DB::table('employee')->select()->whereDay('BirthDate', '04')->get();
+        // dd($data);
+
+        // whereyear
+        // $data = DB::table('employee')->select()->whereyear('BirthDate', '2020')->get();
+        // dd($data);
+
+        // return the component of url
+        // $url = "https://www.geeksforgeeks.org/php-parse_url-function/";
+        // echo "<pre>";
+        // print_r(parse_url($url));
+        // die;
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        // 1) orderby(colname,asc/desc)
+        // $data = DB::table('employee')->select()->orderBy('EmployeeId','desc')->get();
+        // dd($data);
+
+        // 2) latest(colname) => date pramane result return karse. je latest date haise teno data apse
+        // $data = DB::table('employee')->select()->latest('BirthDate')->first();
+        // dd($data);
+
+        // 3)oldest(colname) => date pramane je oldest date haise teno record return karse.
+        // $data = DB::table('employee')->select()->oldest('BirthDate')->first();
+        // dd($data);
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        # groupby
+
+        //$data = DB::table('employee')->selectRaw('Department,avg(salary) As Salary')->groupBy('Department')->having('Salary','>',30000)->get();
+        // $data = DB::table('employee')->selectRaw('Gender,count(EmployeeId)')->groupBy('Gender')->get();
+        //$data = DB::table('employee')->selectRaw('Department,count(Gender)')->groupBy('Department','Gender')->having('Gender','=','Male')->get();
+        //  dd($data);
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        # Limit() & Offset()  / Limit() = take() And Offset() ->skip()
+
+        //1) limit(value)
+        // $data = DB::table('employee')->select()->limit(5)->get();
+        // dd($data);
+
+        // 2) offset(value)
+        //  $data = DB::table('employee')->select()->offset(3)->limit(5)->get();
+        //  dd($data);
+
+        // 3) take(value)
+        // $data = DB::table('employee')->select()->take(5)->get();
+        // dd($data);
+
+        // 4) skip(value)
+        // $data = DB::table('employee')->select()->skip(2)->take(5)->get();
         // dd($data);
 
         //return view('DatabaseData/querybuilder',['data'=>$data]);
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        # Insert Record into table
+
+        //1)insert single record into table
+        // $data = DB::table('employee')->insert([
+        //      "EmployeeId" => 1012,
+        //      "FullName" => "Manan Shah",
+        //      "Department" => "OpenSource",
+        //      "Salary" => 65000,
+        //      "Gender" => "Male",
+        //      "Age" => 22,
+        //      "BirthDate" => "2020-08-12"
+        // ]);
+        // dd($data);
+
+        //2) Insert Multiple Record
+        // $data = DB::table('employee')->insert([
+        //   ["EmployeeId" => 1013,"FullName" => "Ravi Mandani","Department" => "Frontend","Salary" => 150000,"Gender" => "Male","Age" => 34,"BirthDate" => "2001-06-04"],
+        //   ["EmployeeId" => 1014,"FullName" => "Rutul Sheladiya","Department" => "Backend","Salary" => 140000,"Gender" => "Male","Age" => 24,"BirthDate" => "2004-12-23"],
+        //   ["EmployeeId" => 1015,"FullName" => "Purvish Dhameliya","Department" => "Frontend","Salary" => 250000,"Gender" => "Male","Age" => 74,"BirthDate" => "2010-03-18"],
+        //   ["EmployeeId" => 1016,"FullName" => "Sumit Rajput","Department" => "Backend","Salary" => 550000,"Gender" => "Male","Age" => 74,"BirthDate" => "1998-12-20"]
+        // ]);
+
+        // 3) insertIgnore() => jyare koi id pk che and alredy add thayeli d upr jo insert kariye to constraint violation ni error ave. te error ne ignore karva mate insertIgnore() no use thase.
+        // $data = DB::table('employee')->insertOrIgnore([
+        //      "EmployeeId" => 1001,
+        //      "FullName" => "Kerul Shah",
+        //      "Department" => "HR",
+        //      "Salary" => 65000,
+        //      "Gender" => "Male",
+        //      "Age" => 90,
+        //      "BirthDate" => "2023-12-09"
+        // ]);
+        // dd($data);
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        # Update Record into table
+        // $data = DB::table('employee')->where('EmployeeId','=',1006)->update([
+        //     "FullName" => "Mansi Patel",
+        //     "Department" => "HR"
+        // ]);
+        // dd($data);
+
+        // --------------------------------------------------------------------------------------------------------------------
+        # Joins
+
+        // 1) Innerjoin
+
+        // $data = DB::table('Employee AS e')
+        // ->select('e.EmployeeId','e.FullName','e.Department','a.City','a.State','a.Country')
+        // ->join('Address AS a','e.EmployeeId','=','a.EmployeeId')
+        // ->get();
+        // echo "<pre>";
+        // print_r($data);
+
+
+        //  $data = DB::table('Employee')
+        //  ->select('Employee.EmployeeId','Employee.FullName','Employee.Department','Address.City','Address.State','Address.Country')
+        //  ->join('Address','Employee.EmployeeId','=','Address.EmployeeId')
+        //  ->join('Projects','Address.EmployeeId','=','Projects.EmployeeId')
+        //  ->orderBy('Employee.EmployeeId')
+        //  ->get();
+        //  echo "<pre>";
+        //  print_r($data);
+
+        // 2) LeftJoin
+        // $data = DB::table('Employee As e')
+        // ->select('e.EmployeeId','e.FullName','e.Department','p.ProjectName','p.StartDate','p.EndDate')
+        // ->leftJoin('Projects As p','e.EmployeeId','=','p.EmployeeId')
+        // ->get();
+        // echo "<pre>";
+        // print_r($data);
+
+        //3) RightJoin
+        // $data = DB::table('Employee As e')
+        // ->select('e.EmployeeId','e.FullName','e.Department','p.ProjectName','p.StartDate','p.EndDate')
+        // ->rightJoin('Projects As p','e.EmployeeId','=','p.EmployeeId')
+        // ->get();
+        // echo "<pre>";
+        // print_r($data);
+
+        //4) cross join
+        // $data = DB::table('Employee AS e')->crossJoin('Address AS a')->orderBy('e.EmployeeId','ASC')->get();
+        // echo "<pre>";
+        // print_r($data);
+
+        //5)Natural join => natural join means banne table ma koi aek field common hovi joiye banne table ma koi aek field ni name same hovu joiye
     }
 }
