aa7bfd1e52230127e2bcd8ae14237f70152c7fe5
RutulSheladiya <93831407+Rutul-Sheladiya@users.noreply.github.com>
2023-06-25 01:54:49 +0530

    start working on migrations create student and register migration

--- database/migrations/2023_06_24_190133_create_student_table.php
+++ database/migrations/2023_06_24_190133_create_student_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tests', function (Blueprint $table) {
+        Schema::create('student', function (Blueprint $table) {
             $table->id();
+            $table->string('studentname',50);
+            $table->integer('mobileno');
+            $table->string('address',50);
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tests');
+        Schema::dropIfExists('student');
     }
 };
