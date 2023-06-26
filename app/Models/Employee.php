e3110c40f647f8057ce276a97a99906b2a814401
rutul sheladiya <rutul@simformsolutions.com>
2023-06-26 19:37:29 +0530

    seeding completed. addedd fake datausing faker, added json data in database

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,5 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
-    public $table = "employee";
 }
---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Employee;
+use Illuminate\Support\Facades\File;
+
+class EmployeeSeeder extends Seeder
+
+
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // JSON Data
+        // $jsonData = File::get('database/data/employee.json');
+        // $empData = collect(json_decode($jsonData));
+
+        // $empData->each(function ($emp) {
+        //     employee::create([
+        //         'name' => $emp->name,
+        //         'email' => $emp->email,
+        //         'password' => $emp->password,
+        //         'mobileno' =>$emp->mobileno,
+        //         'birthdate' => $emp->birthdate,
+        //         'city' => $emp->city,
+        //         'state' => $emp->state,
+        //         'country' => $emp->country
+        //     ]);
+        // });
+
+
+        // ==============================================================
+
+        // JSON Data
+        $jsonData = File::get('database/data/employee.json');
+        $empData = json_decode($jsonData, true);
+
+        foreach ($empData as $emp) {
+            employee::create([
+                'name' => $emp['name'],
+                'email' => $emp['email'],
+                'password' => $emp['password'],
+                'mobileno' => $emp['mobileno'],
+                'birthdate' => $emp['birthdate'],
+                'city' => $emp['city'],
+                'state' => $emp['state'],
+                'country' => $emp['country']
+            ]);
+        }
+
+
+        //==============================================================
+
+
+        // Fake Data
+        // for($i=1;$i<20000;$i++){
+        //     employee::create([
+        //         'name'=>fake()->name(),
+        //         'email'=>fake()->email(),
+        //         'password'=>fake()->md5(),
+        //         'mobileno'=>fake()->phoneNumber(),
+        //         'birthdate'=>fake()->date(),
+        //         'city'=>fake()->city(),
+        //         'state'=>fake()->state(),
+        //         'country'=>fake()->country()
+        //      ]);
+        // }
+
+    }
+}
